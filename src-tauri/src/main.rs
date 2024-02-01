0be85e16d1a693119d85536e5e65c6bdc4c55914
Matěj Pešl <matejpesl1@gmail.com>
2024-02-01 15:12:12 +0100

    feat: add some informational comments

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,3 +13,15 @@ fn main() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+
+// NOTES ----------------------
+// Invoke commands directly from JS: 
+//
+// https://tauri.app/v1/guides/getting-started/setup/integrate#invoke-commands
+//
+// This is how You call it:
+// invoke('greet', { name: 'World' })
+// `invoke` returns a Promise
+// .then((response) => console.log(response)) 
+
